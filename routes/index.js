6f471b5cb108d56bd84bc149a7d4de894459de02
Eduardo Pérez <eduardojoseperezf@gmail.com>
2020-12-02 20:47:25 -0300

    Basic result display

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,13 @@ router.get('/', async (req, res, next) => {
 router.get('/acerca-de', async (req, res, next) => {
   res.render('acerca-de', { title: 'BNDS' });
 });
-router.post('/resultado', async (req, res) => {
 
+router.post('/resultado', async (req, res) => {
+  console.log(req.body);
   //Obtenemos la operadora
   operator = await models.operator.findOne({
     where: {
-      name: req.body.operator
+      id: req.body.operator
     },
     attributes: ['id', 'name', 'urlWeb', 'urlLogo'],
   });
@@ -25,7 +26,7 @@ router.post('/resultado', async (req, res) => {
   //Obtenemos el smartphone
   smartphone = await models.smartphone.findOne({
     where: {
-      fullName: req.body.name
+      fullName: req.body.smartphone
     },
     raw:true
   });
@@ -122,14 +123,22 @@ router.post('/resultado', async (req, res) => {
       });
     }
   }
-
-  // TEMP: Mostramos los resultados en JSON. Deberían ir como parámetros al render
-  res.status(200).json({
+  console.log(generations);
+/*
+  res.json({title: 'BNDS',
+  operator: operator,
+  smartphone: smartphone,
+  generations: genList,
+  technologies: technologies})
+  */
+  res.render('result', {
+    title: 'BNDS',
     operator: operator,
     smartphone: smartphone,
-    frequencies: genList,
+    generations: genList,
     technologies: technologies
   });
+
 })
 
 module.exports = router;
